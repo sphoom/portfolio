8005759ff54bec0e59e462ec2d6c702102f2becc
sphoom <marks.somphoom@gmail.com>
2025-03-16 13:05:54 +0700

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,36 +1,300 @@
-// index.jsx
-import React from "react"; 
-import { Education } from "./Education";
-import { Experience } from "./Experience";
-import { FeaturedProjects } from "./FeaturedProjects";
-import { Navbar } from "./Navbar";
-import { Cover } from "./Cover";
-import "./style.css";
-
-export const PortfolioDark = () => {
-  return (
-    <div className="portfolio-dark">
-      <Navbar />
-      <Cover />
-      <div className="container">
-        <h1 className="title">Your Name</h1>
-        <h2 className="subtitle">Hey, I’m</h2>
-        <p className="description">
-          I'm a data scientist and product analyst passionate about finance and marketing analytics.
-          My projects focus on A/B testing, predictive modeling, and data-driven decision-making.
-        </p>
-        <div className="contact-buttons">
-          <a href="mailto:your-email@example.com" className="btn">Email</a>
-          <a href="https://github.com/yourprofile" className="btn">GitHub</a>
-          <a href="https://linkedin.com/in/yourprofile" className="btn">LinkedIn</a>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="cover">
+      <div class="div">
+        <div class="group">
+          <div class="portoflio-dark">
+            <div class="overlap">
+              <div class="overlap-group">
+                <img class="line" src="img/line-4.svg" />
+                <div class="frame">
+                  <div class="frame-2">
+                    <div class="text-wrapper">Software Developer (Remote)</div>
+                    <div class="text-wrapper-2">Jun 2021 - Present</div>
+                  </div>
+                  <p class="selfbook-US-new-york">Selfbook / US - New York</p>
+                  <p class="p">
+                    - Developing screens and UI components for the web application using React and Tailwind.
+                  </p>
+                  <p class="p">- Fixing UI issues and integrating backend APIs with Redux Saga.</p>
+                </div>
+                <div class="line-wrapper"><img class="img" src="img/line-6.svg" /></div>
+                <div class="text-wrapper-3">Selfbook</div>
+              </div>
+              <div class="text-wrapper-4">Wevoz</div>
+              <div class="text-wrapper-5">FreeBeings</div>
+            </div>
+            <div class="text-wrapper-6">Darlene Robertson</div>
+            <div class="text-wrapper-7">Hey, I’m</div>
+            <p class="i-m-a-software">
+              I&#39;m a software engineer based in Toronto, Canada and also a communication and journalism student. I
+              enjoy creating things that live on the internet, whether that be websites, applications, or anything in
+              between. I have been freelancing for a year now while studying at the university and I&#39;ve manage to
+              gain a decent amount of experience and valuable knowledge from all different kinds of fields throughout my
+              projects/work.
+            </p>
+            <div class="navigation-bar-dark">
+              <div class="frame-wrapper">
+                <div class="frame-3">
+                  <div class="text-wrapper-8">DR.</div>
+                  <div class="frame-4">
+                    <div class="text-wrapper-9">Projects</div>
+                    <div class="text-wrapper-9">Resume</div>
+                    <div class="text-wrapper-9">Contact</div>
+                    <div class="sun-fill"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <img class="line-2" src="img/line-5.svg" />
+            <div class="frame-5">
+              <div class="overlap-group-wrapper">
+                <div class="overlap-group-2">
+                  <img class="vector" src="img/vector-4.svg" />
+                  <div class="text-wrapper-10">Send an email</div>
+                </div>
+              </div>
+              <div class="btn-linkedin-outline">
+                <div class="div-wrapper">
+                  <div class="overlap-group-3">
+                    <div class="text-wrapper-11">LinkedIn</div>
+                    <img class="vector-2" src="img/vector-7.svg" />
+                  </div>
+                </div>
+              </div>
+              <div class="overlap-group-wrapper">
+                <div class="overlap-2">
+                  <div class="text-wrapper-12">Upwork</div>
+                  <img class="vector-3" src="img/vector-14.svg" />
+                </div>
+              </div>
+              <div class="btn-github-outline">
+                <div class="overlap-3">
+                  <div class="text-wrapper-13">Github</div>
+                  <img class="vector-4" src="img/vector-9.svg" />
+                </div>
+              </div>
+            </div>
+            <div class="experience">EXPERIENCE</div>
+            <div class="featured-projects">FEATURED PROJECTS</div>
+            <div class="more-projects">MORE PROJECTS</div>
+            <img class="line-3" src="img/line-8.svg" />
+            <div class="text-wrapper-14">TDF</div>
+            <div class="text-wrapper-15">Upwork</div>
+            <div class="text-wrapper-16">Shoppy</div>
+            <div class="gradients-sugar"></div>
+            <div class="gradients-sugar-2"></div>
+            <div class="gradients-sugar-3"></div>
+            <div class="gradients-sugar-4"></div>
+            <div class="gradients-sugar-5"></div>
+            <div class="gradients-sugar-6"></div>
+            <div class="gradients-sugar-7"></div>
+            <div class="gradients-sugar-8"></div>
+            <div class="gradients-sugar-9"></div>
+            <div class="frame-6">
+              <div class="text-wrapper-17">IntenseProxy</div>
+              <p class="text-wrapper-18">
+                Proxy provider website including authentication, dashboard and dynamic features
+              </p>
+              <p class="text-wrapper-19">React - Bootstrap - Styled Components</p>
+              <div class="frame-7">
+                <div class="group-2">
+                  <div class="overlap-group-4">
+                    <img class="group-3" src="img/group-12.png" />
+                    <img class="vector-5" src="img/vector-5.svg" />
+                    <img class="gradients-purple-sky" src="img/purple-sky-eased.svg" />
+                  </div>
+                  <div class="text-wrapper-20">Live demo</div>
+                </div>
+              </div>
+            </div>
+            <div class="group-4">
+              <div class="frame-8">
+                <div class="text-wrapper-21">Vue Weather APP</div>
+                <p class="text-wrapper-18">
+                  My first Vue app using OpenWeather API and fetch API with no custom libraries for requests or styling.
+                </p>
+                <div class="text-wrapper-22">Vue.js</div>
+                <div class="frame-7">
+                  <div class="group-2">
+                    <div class="overlap-group-5">
+                      <img class="group-3" src="img/group-14.png" />
+                      <img class="vector-5" src="img/vector-2.svg" />
+                      <img class="gradients-purple-sky" src="img/purple-sky-eased-5.svg" />
+                    </div>
+                    <div class="text-wrapper-20">Live demo</div>
+                  </div>
+                </div>
+              </div>
+              <div class="frame-9">
+                <div class="text-wrapper-23">Vuewinder</div>
+                <p class="used-all-what-i-was">
+                  Used all what I was able to find publicly that is similar to Twitter&#39;s UI and also added the tweet
+                  function
+                </p>
+                <div class="text-wrapper-22">Vue.js - TailwindCSS</div>
+                <div class="frame-10">
+                  <div class="group-5">
+                    <div class="overlap-group-6">
+                      <img class="group-3" src="img/group-15.png" />
+                      <img class="vector-5" src="img/vector-13.svg" />
+                      <img class="gradients-purple-sky" src="img/purple-sky-eased-7.svg" />
+                    </div>
+                    <div class="text-wrapper-20">Live demo</div>
+                  </div>
+                  <img class="vector-6" src="img/vector-12.svg" />
+                </div>
+                <div class="frame-11"></div>
+              </div>
+            </div>
+            <div class="frame-12">
+              <div class="text-wrapper-24">$AAPE Coin</div>
+              <p class="text-wrapper-25">Site for a BSC based coin called $AAPE</p>
+              <p class="text-wrapper-26">Vue - Nuxt - Netlify</p>
+              <div class="frame-7">
+                <div class="group-2">
+                  <div class="overlap-group-7">
+                    <img class="group-3" src="img/group.png" />
+                    <img class="vector-5" src="img/vector-11.svg" />
+                    <img class="gradients-purple-sky" src="img/purple-sky-eased-11.svg" />
+                  </div>
+                  <div class="text-wrapper-20">Live demo</div>
+                </div>
+              </div>
+            </div>
+            <div class="frame-13">
+              <div class="text-wrapper-27">GitProfile</div>
+              <p class="text-wrapper-28">
+                A nicer look to Github profiles using features such as user search, authentication using Firebase and
+                charts using the GitHub API.
+              </p>
+              <p class="text-wrapper-29">React - Redux - Firebase - GitHub API</p>
+              <div class="frame-14">
+                <div class="group-5">
+                  <div class="overlap-group-8">
+                    <img class="group-3" src="img/group-9.png" />
+                    <img class="vector-5" src="img/vector-10.svg" />
+                    <img class="gradients-purple-sky" src="img/purple-sky-eased-9.svg" />
+                  </div>
+                  <div class="text-wrapper-20">Live demo</div>
+                </div>
+                <div class="github-fill"></div>
+              </div>
+            </div>
+            <div class="overlap-4">
+              <div class="frame-15">
+                <div class="text-wrapper-30">Mu6icbot</div>
+                <p class="discord-music-bot">
+                  Discord music bot using Node.js and Discord&#39;s API library to play music through YouTube, added
+                  Genius API making the bot capable to pull the lyrics for the current playing song
+                </p>
+                <p class="text-wrapper-31">Discord Library - Node.js - Genius API - Youtube API</p>
+                <div class="frame-7"><div class="github-fill-2"></div></div>
+              </div>
+              <div class="frame-16">
+                <div class="text-wrapper-17">IntenseProxy</div>
+                <p class="text-wrapper-18">
+                  Proxy provider website including authentication, dashboard and dynamic features
+                </p>
+                <p class="text-wrapper-19">React - Bootstrap - Styled Components</p>
+                <div class="frame-14">
+                  <div class="group-5">
+                    <div class="overlap-group-9">
+                      <img class="group-3" src="img/group-3.png" />
+                      <img class="vector-5" src="img/vector-6.svg" />
+                      <img class="gradients-purple-sky" src="img/purple-sky-eased-3.svg" />
+                    </div>
+                    <div class="text-wrapper-20">Live demo</div>
+                  </div>
+                  <div class="github-fill-3"></div>
+                </div>
+              </div>
+              <img class="line-4" src="img/line-9.svg" />
+            </div>
+            <div class="frame-17">
+              <div class="text-wrapper-32">MovieDB</div>
+              <p class="text-wrapper-33">
+                TV application, displays different categories of movies/shows (popular, in theaters, trending etc)
+                alongside a search option using TheMovieDB API for the data
+              </p>
+              <p class="text-wrapper-34">React.js - Redux - TheMovieDB API</p>
+              <div class="frame-14">
+                <div class="group-5">
+                  <div class="overlap-group-10">
+                    <img class="group-3" src="img/group-10.png" />
+                    <img class="vector-5" src="img/vector-8.svg" />
+                    <img class="gradients-purple-sky" src="img/purple-sky-eased-6.svg" />
+                  </div>
+                  <div class="text-wrapper-20">Live demo</div>
+                </div>
+                <div class="github-fill-4"></div>
+              </div>
+            </div>
+            <div class="frame-18">
+              <p class="text-wrapper-35">Blockchain Explorer; Hive Attention Tokens</p>
+              <p class="text-wrapper-36">Sidechain explorer open source project (block explorer) for transactions.</p>
+              <p class="text-wrapper-37">React - TailwindCSS - Context API</p>
+              <div class="frame-14">
+                <div class="group-5">
+                  <div class="overlap-group-11">
+                    <img class="group-3" src="img/group-6.png" />
+                    <img class="vector-5" src="img/vector-3.svg" />
+                    <img class="gradients-purple-sky" src="img/purple-sky-eased-2.svg" />
+                  </div>
+                  <div class="text-wrapper-20">Live demo</div>
+                </div>
+                <div class="github-fill-5"></div>
+              </div>
+            </div>
+            <div class="frame-19">
+              <div class="text-wrapper-38">Soundify</div>
+              <p class="text-wrapper-39">
+                Music search application, displays informations about artists such as albums, biography, songs that are
+                available on youtube and much more using TheAudioDB API
+              </p>
+              <p class="text-wrapper-40">Vue.js - Vuex - TheAudioDB API</p>
+              <div class="frame-14">
+                <div class="group-5">
+                  <div class="overlap-group-12">
+                    <img class="group-3" src="img/group-8.png" />
+                    <img class="vector-5" src="img/vector.svg" />
+                    <img class="gradients-purple-sky" src="img/purple-sky-eased-4.svg" />
+                  </div>
+                  <div class="text-wrapper-20">Live demo</div>
+                </div>
+                <div class="github-fill-6"></div>
+              </div>
+            </div>
+            <div class="frame-20">
+              <div class="text-wrapper-17">IntenseProxy</div>
+              <p class="text-wrapper-18">
+                Proxy provider website including authentication, dashboard and dynamic features
+              </p>
+              <p class="text-wrapper-19">React - Bootstrap - Styled Components</p>
+              <div class="frame-14">
+                <div class="group-5">
+                  <div class="overlap-group-13">
+                    <img class="group-3" src="img/group-5.png" />
+                    <img class="vector-5" src="img/vector-15.svg" />
+                    <img class="gradients-purple-sky" src="img/purple-sky-eased-8.svg" />
+                  </div>
+                  <div class="text-wrapper-20">Live demo</div>
+                </div>
+                <div class="github-fill-7"></div>
+              </div>
+            </div>
+          </div>
         </div>
-        <FeaturedProjects />
-        <Education />
-        <Experience />
+        <p class="portfolio-UI-design">
+          <span class="span">Portfolio UI Design<br /></span> <span class="text-wrapper-41">Template</span>
+        </p>
       </div>
-      <footer className="footer">&copy; 2025 Your Name. All Rights Reserved.</footer>
     </div>
-  );
-};
-
-export default PortfolioDark;
+  </body>
+</html>
